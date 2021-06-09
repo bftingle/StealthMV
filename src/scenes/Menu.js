261ece8a1bf6a2cf6343ca752f9f8a91c052d62c
sthiru224 <sthiru@ucsc.edu>
2021-06-08 19:20:12 -0700

    dev play restored

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,6 +9,7 @@ class Menu extends Phaser.Scene {
         this.load.image('rules', './assets/rules.png');
         this.load.image('play', './assets/play.png');
         this.load.image('button', './assets/button2.png');
+        this.load.image('grader_button', './assets/grader_button.png');
         this.load.audio('menuclick1', './assets/menuclick1.wav');
         this.load.audio('menuclick2', './assets/menuclick2.wav');
         this.load.audio('play_music', './assets/Gameplay.wav');
@@ -18,7 +19,8 @@ class Menu extends Phaser.Scene {
         this.add.sprite(700,430,"intro");
         new Button(this, 200, 432, 'credits', 0, '', '36px', 'gotoCredits').setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
         new Button(this, 680, 432, 'play', 0, '', '36px', 'gotoPlay').setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
-        new Button(this, 1200, 432, 'rules', 0, '', '20px', 'gotoDevPlay').setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
+        new Button(this, 1200, 432, 'rules', 0, '', '20px', 'gotoInstructions').setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
+        new Button(this, 680, 530, 'grader_button', 0, '', '20px', 'gotoDevPlay').setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
         //new Button(this, 1200, 432, 'rules', 0, '', '20px', 'gotoInstructions').setOrigin(0.5, 0.5).setInteractive({useHandCursor: true});
         
 
